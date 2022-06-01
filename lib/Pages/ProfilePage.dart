6fb90ba1601b17533905ce4dea91841bdd6f0909
S1K4RA <73513381+S1K4RA@users.noreply.github.com>
2022-06-01 22:48:35 +0700

    profile page

--- lib/Pages/ProfilePage.dart
+++ lib/Pages/ProfilePage.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({ Key? key }) : super(key: key);
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -15,6 +15,79 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Email"),
+                IconButton(onPressed: null, icon: Icon(Icons.edit), iconSize: 24,)
+              ],
+            ),
+            TextField(
+              enabled: false,
+              readOnly: true,
+              controller: null,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.all(8),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top:20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Password"),
+                IconButton(onPressed: null, icon: Icon(Icons.edit), iconSize: 24,)
+              ],
+            ),
+            TextField(
+              enabled: false,
+              readOnly: true,
+              controller: null,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top:20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Nomor Telepon"),
+                IconButton(onPressed: null, icon: Icon(Icons.edit), iconSize: 24,)
+              ],
+            ),
+            TextField(
+              enabled: false,
+              readOnly: true,
+              controller: null,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top:20)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Alamat"),
+                IconButton(onPressed: null, icon: Icon(Icons.edit), iconSize: 24,)
+              ],
+            ),
+            TextField(
+              enabled: false,
+              readOnly: true,
+              controller: null,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
