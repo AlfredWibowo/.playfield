9d95b729425112b95aae39ee8bbfdeda2a8bff39
S1K4RA <73513381+S1K4RA@users.noreply.github.com>
2022-06-19 19:39:19 +0700

    conflict resolved

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class SplashScreen extends StatelessWidget {
       home: AnimatedSplashScreen(
         duration: 500,
         splash: Icons.sports_handball,
-        nextScreen: MyApp(),
+        nextScreen: WelcomePage(),
         splashTransition: SplashTransition.fadeTransition,
         pageTransitionType: PageTransitionType.fade,
         backgroundColor: Colors.white,
