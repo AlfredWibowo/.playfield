1bbb8c57d995c2dca13f18195d0ccbb9ac8a5862
S1K4RA <73513381+S1K4RA@users.noreply.github.com>
2022-06-28 07:54:22 +0700

    rehat bentar

--- lib/pages/admin/MaintenancePage.dart
+++ lib/pages/admin/MaintenancePage.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:project_ambw/class/CUserSession.dart';
 import 'package:project_ambw/functions/widget.dart';
+import 'package:project_ambw/pages/admin/manage/sportCenterPage.dart';
 
 class AdminMaintenancePage extends StatefulWidget {
   const AdminMaintenancePage({Key? key}) : super(key: key);
@@ -21,6 +23,61 @@ class _AdminMaintenancePageState extends State<AdminMaintenancePage> {
           children: [
             title('Manage your', false),
             title('Sports center', true),
+            SizedBox(height: 32.0),
+            Expanded(
+                child: ListView.builder(
+              itemCount: AdminSession.session.owns.length,
+              itemBuilder: (context, index) {
+                return sportCenterCard(
+                    AdminSession.session.owns[index].nama,
+                    AdminSession.session.owns[index].alamat,
+                    AdminSession.session.owns[index].kota,
+                    context, index);
+              },
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget sportCenterCard(
+      String scName, String scAddress, String scCity, BuildContext context, int index) {
+    return Card(
+      color: Colors.black,
+      child: InkWell(
+        onTap: () {
+          // Tp to Page
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SCDetailPage(index: index),
+              )
+          );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    scName,
+                    style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontFamily: 'Comfortaa'),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  textWithIconRow(Icons.location_on, scAddress),
+                  textWithIconRow(Icons.location_city, scCity),
+                ],
+              ),
+            ),
           ],
         ),
       ),
