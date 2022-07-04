668430731527b1071256bf65d6c8eb838ca916bf
AlfredWibowo <wibowoalfred@gmail.com>
2022-07-04 16:58:29 +0700

    add Notification class

---
+++ lib/class/Notification.dart
@@ -0,0 +1,11 @@
+class Notification {
+  String date;
+  String message;
+  bool readed;
+
+  Notification({
+    required this.date,
+    required this.message,
+    required this.readed,
+  });
+}
